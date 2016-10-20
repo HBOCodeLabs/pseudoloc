826bdb7d18105cddb44d10391ff2a89a32722fec
Zack Birkenbuel <zack.birkenbuel@hbo.com>
2016-10-20 16:32:10 -0700

    copyright comments and quote normalization

--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -1,18 +1,78 @@
+/**
+ * @preserve Copyright (c) 2015 Home Box Office, Inc. as an unpublished
+ * work. Neither this material nor any portion hereof may be copied or
+ * distributed without the express written consent of Home Box Office, Inc.
+ *
+ * This material also contains proprietary and confidential information
+ * of Home Box Office, Inc. and its suppliers, and may not be used by or
+ * disclosed to any person, in whole or in part, without the prior written
+ * consent of Home Box Office, Inc.
+ */
+
+'use strict';
 
 var Promise = require('bluebird');
 var handlers = require('../lib/handlers');
 var lib = require('../lib');
 
-describe("main entry point", function () {
-    it("validates input before handing off to actual handlers", function (done) {
-        spyOn(handlers, "json").and.returnValue(Promise.fulfilled());
-        spyOn(handlers, "po").and.returnValue(Promise.fulfilled());
+describe('validation', function () {
+    beforeEach(function () {
+        spyOn(handlers, 'json').and.returnValue(Promise.fulfilled());
+        spyOn(handlers, 'po').and.returnValue(Promise.fulfilled());
+    });
 
-        var data = ""; // doesn't matter
-        var options = {};
-        lib.json(data, {}).then(function() {
-            expect(handlers.json).toHaveBeenCalledWith(data, options);
-            done();
+    [
+        'json',
+        'po'
+    ].forEach(function (method) {
+        it('rejects if no options parameter has been provided (' + method + ')', function (done) {
+            lib[method]('', undefined).catch(function (err) {
+                expect(err).toBeDefined();
+                done();
+            });
+        });
+
+        it('requires no on options - assumes pseudo localization (' + method + ')', function (done) {
+            var data = ''; // doesn't matter
+            var options = {};
+            lib[method](data, options).then(function () {
+                expect(handlers[method]).toHaveBeenCalledWith(data, options);
+                done();
+            });
+        });
+
+        it('requires an apiKey for google translation (' + method + ')', function (done) {
+            var options = {
+                method: 'google'
+            };
+            lib[method]('', options).catch(function (err) {
+                expect(err).toBeDefined();
+                done();
+            });
+        });
+
+        it('requires a targetLanguage for google translation (' + method + ')', function (done) {
+            var options = {
+                method: 'google',
+                apiKey: 'apiKey'
+            };
+            lib[method]('', options).catch(function (err) {
+                expect(err).toBeDefined();
+                done();
+            });
+        });
+
+        it('calls through as expected if provided all google paramters (' + method + ')', function (done) {
+            var data = ''; // doesn't matter
+            var options = {
+                method: 'google',
+                apiKey: 'apiKey',
+                targetLanguage: 'es'
+            };
+            lib[method]('', options).then(function () {
+                expect(handlers[method]).toHaveBeenCalledWith(data, options);
+                done();
+            });
         });
     });
 });
